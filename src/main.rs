02f56c121f46f360971729d0057499ac78de8f2a
ruuiisu <luis.gjokaj@gmail.com>
2023-10-30 15:36:11 +0100

    read file and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io:: {
+    Result,
+    prelude::*,
+    BufReader
+};
+
+
+fn main() -> Result<()> {
+    let file = File::open("./docs.gl/gl4/glGetCompressedTexImage.xhtml")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    println!("{contents}");
+    Ok(())
 }
