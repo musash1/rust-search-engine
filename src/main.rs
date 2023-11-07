76a4c26dce330f39f943ddb7feb456b16917d3f8
musash1 <luis.gjokaj@gmail.com>
2023-11-07 15:54:36 +0100

    get all files in directory

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use std::fs::File;
-use std::io:: {
-    Result,
-    prelude::*,
-    BufReader
-};
+use std::{fs, path::Path};
 
+fn get_dir(dir_path: &Path) -> Result<(), ()> {
+    let dir = fs::read_dir(dir_path).map_err(|err| {
+        eprintln!("ERROR: could not open directory {dir_path}: {err}", dir_path = dir_path.display());
+    })?; 
 
-fn main() -> Result<()> {
-    let file = File::open("./docs.gl/gl4/glGetCompressedTexImage.xhtml")?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
-    println!("{contents}");
+    'next_file: for file in dir {
+        let file = file.map_err(|err| {
+            eprintln!("ERROR: could not read next file in dir {dir_path}: {err}", dir_path = dir_path.display());
+        })?; 
+
+        let file_path = file.path();
+
+        let file_type = file.file_type().map_err(|err| {
+            eprintln!("ERROR: could not determine type of file {file_path}: {err}", file_path = file_path.display());
+        })?;
+
+        if file_type.is_dir() {
+            get_dir(&file_path);
+            continue 'next_file;
+        }
+
+        println!("Name: {}", file_path.display());
+    }
+
     Ok(())
+}
+
+fn main() {
+    get_dir(Path::new("./docs.gl")); 
 }
